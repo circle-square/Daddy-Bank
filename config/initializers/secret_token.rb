fa74dd98331662e10ccd798437ce36f87785092a
Dean Stevens <ruby@circle-square.co.uk>
2012-10-01 20:33:17 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaddyBank::Application.config.secret_token = 'bf00fd19519912c7687398c1a086b0d06b8395eb79165b2eeaae51bc4bd9a33cc66e9a75eb57b9b1f0a49b972c404d2dbec20a7f7f4747209b7d2f46d0800f60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DaddyBank::Application.config.session_store :cookie_store, key: '_DaddyBank_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DaddyBank::Application.config.session_store :active_record_store
